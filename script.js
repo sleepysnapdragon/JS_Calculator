b967cf8a3e8b29caaec32833cf61ea9719acfc26
Gillian <gillian@yeomans.org.uk>
2019-10-03 15:39:29 +0200

    design upgrade, calculations work

--- script.js
+++ script.js
@@ -21,22 +21,7 @@ var displayValue = [];
 
 //add a number button class in the html and create vars with a loop?
 
-//var numberButtons = [zero, one, two, three, four, five, six, seven, eight, nine];
-
-var numberButtons = [
-  {
-    element: zero,
-    value: 0
-  },
-  {
-    element: one,
-    value: 1
-  },
-  {
-    element: two,
-    value: 2
-  }
-];
+//try and create something more efficient like an array of objects?
 
 /* can't pass value of each object into function (or need to do something complicated to do so (this/bind?))
 for (var i = 0; i < numberButtons.length; i++) {
@@ -44,122 +29,6 @@ for (var i = 0; i < numberButtons.length; i++) {
   );
 }
 */
-
-function calc(val) {
-  display.innerHTML = val;
-}
-
-clear.addEventListener("click", reset);
-
-function pressZero() {
-  if (typeof calculation[calculation.length - 1] == "number") {
-    displayValue.push(0);
-  } else {
-    displayValue = [0];
-  }
-  calculation.push(0);
-  display.innerHTML = displayValue.join("");
-  console.log(calculation);
-}
-
-function pressOne() {
-  if (typeof calculation[calculation.length - 1] == "number") {
-    displayValue.push(1);
-  } else {
-    displayValue = [1];
-  }
-  calculation.push(1);
-  display.innerHTML = displayValue.join("");
-  console.log(calculation);
-}
-
-function pressTwo() {
-  if (typeof calculation[calculation.length - 1] == "number") {
-    displayValue.push(2);
-  } else {
-    displayValue = [2];
-  }
-  calculation.push(2);
-  display.innerHTML = displayValue.join("");
-  console.log(calculation);
-}
-
-function pressThree() {
-  if (typeof calculation[calculation.length - 1] == "number") {
-    displayValue.push(3);
-  } else {
-    displayValue = [3];
-  }
-  calculation.push(3);
-  display.innerHTML = displayValue.join("");
-  console.log(calculation);
-}
-
-function pressFour() {
-  if (typeof calculation[calculation.length - 1] == "number") {
-    displayValue.push(4);
-  } else {
-    displayValue = [4];
-  }
-  calculation.push(4);
-  display.innerHTML = displayValue.join("");
-  console.log(calculation);
-}
-
-function pressFive() {
-  if (typeof calculation[calculation.length - 1] == "number") {
-    displayValue.push(5);
-  } else {
-    displayValue = [5];
-  }
-  calculation.push(5);
-  display.innerHTML = displayValue.join("");
-  console.log(calculation);
-}
-
-function pressSix() {
-  if (typeof calculation[calculation.length - 1] == "number") {
-    displayValue.push(6);
-  } else {
-    displayValue = [6];
-  }
-  calculation.push(6);
-  display.innerHTML = displayValue.join("");
-  console.log(calculation);
-}
-
-function pressSeven() {
-  if (typeof calculation[calculation.length - 1] == "number") {
-    displayValue.push(7);
-  } else {
-    displayValue = [7];
-  }
-  calculation.push(7);
-  display.innerHTML = displayValue.join("");
-  console.log(calculation);
-}
-
-function pressEight() {
-  if (typeof calculation[calculation.length - 1] == "number") {
-    displayValue.push(8);
-  } else {
-    displayValue = [8];
-  }
-  calculation.push(8);
-  display.innerHTML = displayValue.join("");
-  console.log(calculation);
-}
-
-function pressNine() {
-  if (typeof calculation[calculation.length - 1] == "number") {
-    displayValue.push(9);
-  } else {
-    displayValue = [9];
-  }
-  calculation.push(9);
-  display.innerHTML = displayValue.join("");
-  console.log(calculation);
-}
 
 zero.addEventListener("click", pressZero);
 one.addEventListener("click", pressOne);
@@ -177,6 +46,168 @@ subtract.addEventListener("click", pressSubtract);
 multiply.addEventListener("click", pressMultiply);
 divide.addEventListener("click", pressDivide);
 equals.addEventListener("click", pressEquals);
+
+clear.addEventListener("click", reset);
+
+function pressZero() {
+  if (
+    calculation.length !== 0 &&
+    typeof calculation[calculation.length - 1] == "number"
+  ) {
+    displayValue.push(0);
+    calculation[calculation.length - 1] =
+      calculation[calculation.length - 1] * 10 + 0;
+  } else {
+    displayValue = [0];
+    calculation.push(0);
+  }
+  display.innerHTML = displayValue.join("");
+  console.log(calculation);
+}
+
+function pressOne() {
+  if (
+    calculation.length !== 0 &&
+    typeof calculation[calculation.length - 1] == "number"
+  ) {
+    displayValue.push(1);
+    calculation[calculation.length - 1] =
+      calculation[calculation.length - 1] * 10 + 1;
+  } else {
+    displayValue = [1];
+    calculation.push(1);
+  }
+  display.innerHTML = displayValue.join("");
+  console.log(calculation);
+}
+
+function pressTwo() {
+  if (
+    calculation.length !== 0 &&
+    typeof calculation[calculation.length - 1] == "number"
+  ) {
+    displayValue.push(2);
+    calculation[calculation.length - 1] =
+      calculation[calculation.length - 1] * 10 + 2;
+  } else {
+    displayValue = [2];
+    calculation.push(2);
+  }
+  display.innerHTML = displayValue.join("");
+  console.log(calculation);
+}
+
+function pressThree() {
+  if (
+    calculation.length !== 0 &&
+    typeof calculation[calculation.length - 1] == "number"
+  ) {
+    displayValue.push(3);
+    calculation[calculation.length - 1] =
+      calculation[calculation.length - 1] * 10 + 3;
+  } else {
+    displayValue = [3];
+    calculation.push(3);
+  }
+  display.innerHTML = displayValue.join("");
+  console.log(calculation);
+}
+
+function pressFour() {
+  if (
+    calculation.length !== 0 &&
+    typeof calculation[calculation.length - 1] == "number"
+  ) {
+    displayValue.push(4);
+    calculation[calculation.length - 1] =
+      calculation[calculation.length - 1] * 10 + 4;
+  } else {
+    displayValue = [4];
+    calculation.push(4);
+  }
+  display.innerHTML = displayValue.join("");
+  console.log(calculation);
+}
+
+function pressFive() {
+  if (
+    calculation.length !== 0 &&
+    typeof calculation[calculation.length - 1] == "number"
+  ) {
+    displayValue.push(5);
+    calculation[calculation.length - 1] =
+      calculation[calculation.length - 1] * 10 + 5;
+  } else {
+    displayValue = [5];
+    calculation.push(5);
+  }
+  display.innerHTML = displayValue.join("");
+  console.log(calculation);
+}
+
+function pressSix() {
+  if (
+    calculation.length !== 0 &&
+    typeof calculation[calculation.length - 1] == "number"
+  ) {
+    displayValue.push(6);
+    calculation[calculation.length - 1] =
+      calculation[calculation.length - 1] * 10 + 6;
+  } else {
+    displayValue = [6];
+    calculation.push(6);
+  }
+  display.innerHTML = displayValue.join("");
+  console.log(calculation);
+}
+
+function pressSeven() {
+  if (
+    calculation.length !== 0 &&
+    typeof calculation[calculation.length - 1] == "number"
+  ) {
+    displayValue.push(7);
+    calculation[calculation.length - 1] =
+      calculation[calculation.length - 1] * 10 + 7;
+  } else {
+    displayValue = [7];
+    calculation.push(7);
+  }
+  display.innerHTML = displayValue.join("");
+  console.log(calculation);
+}
+
+function pressEight() {
+  if (
+    calculation.length !== 0 &&
+    typeof calculation[calculation.length - 1] == "number"
+  ) {
+    displayValue.push(8);
+    calculation[calculation.length - 1] =
+      calculation[calculation.length - 1] * 10 + 8;
+  } else {
+    displayValue = [8];
+    calculation.push(8);
+  }
+  display.innerHTML = displayValue.join("");
+  console.log(calculation);
+}
+
+function pressNine() {
+  if (
+    calculation.length !== 0 &&
+    typeof calculation[calculation.length - 1] == "number"
+  ) {
+    displayValue.push(9);
+    calculation[calculation.length - 1] =
+      calculation[calculation.length - 1] * 10 + 9;
+  } else {
+    displayValue = [9];
+    calculation.push(9);
+  }
+  display.innerHTML = displayValue.join("");
+  console.log(calculation);
+}
 
 function pressAdd() {
   displayValue = ["+"];
@@ -207,8 +238,10 @@ function pressDivide() {
 }
 
 function pressEquals() {
-  for (var i = 0; i < calculation.length; i++) {}
-  console.log(calculation);
+  var result = eval(calculation.join(""));
+  displayValue = result;
+  display.innerHTML = displayValue;
+  calculation = [];
 }
 
 function reset() {
